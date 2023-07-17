f1301db523759a213db10c32d1f74768780b9170
Sini <139808836+SiniHelin@users.noreply.github.com>
2023-07-17 22:07:07 +0300

    Add files via upload

---
+++ Answer machine.py
@@ -0,0 +1,4 @@
+import random
+answer_list = ['Yes sir' , 'Absolutely not', 'Might aswell, yes.', 'Maybe', 'Well now that i think about it...yes!', '0_0 why even ask that', ':-)']
+question = input("Ask a question: ")
+print(random.choice(answer_list))
